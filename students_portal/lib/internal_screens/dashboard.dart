f5819bf1d3ebbb31a89c724bf3398a71369ece07
Jawed Alam <zjaweds@outlook.com>
2021-10-01 19:08:54 +0530

    Background for Dashboard added

--- students_portal/lib/internal_screens/dashboard.dart
+++ students_portal/lib/internal_screens/dashboard.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:students_portal/internal_screens/header.dart';
 
+class LoggedInCandidateDashboard extends StatelessWidget {
+  const LoggedInCandidateDashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const CustomHeader(),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.752,
+          color: Colors.white,
+        ),
+      ],
+    );
+  }
+}
