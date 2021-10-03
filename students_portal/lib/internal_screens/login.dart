90c7c9e7219d763ffc336050cca7e72914117682
Jawed Alam <zjaweds@outlook.com>
2021-10-03 12:51:23 +0530

    Login Panel without credentials field

--- students_portal/lib/internal_screens/login.dart
+++ students_portal/lib/internal_screens/login.dart
@@ -26,11 +26,62 @@ class LoginFieldHead extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return const Center(
-      child: Text(
-        "STUDENT",
-        style: TextStyle(decoration: null, color: Colors.white, fontSize: 200),
-      ),
+    return Column(
+      
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            color: const Color(0xff39a6f0),
+            child: Center(
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Image.asset("assets/images/studentIcon.png"),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "STUDENT",
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            color: Colors.white,
+                            fontSize: 35),
+                      ),
+                      Text(
+                        "Student Exam Portal",
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            color: Colors.white,
+                            fontSize: 20),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Container(
+          color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                "Login Panel",
+                style: TextStyle(
+                    decorationColor: Colors.redAccent,
+                    decorationThickness: 1,
+                    fontSize: 20,
+                    decorationStyle: TextDecorationStyle.solid,
+                    color: Colors.black),
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
